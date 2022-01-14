1267a85ff2915399137fa25a209adfd3a1571a6c
Kyle Lam <86345484+kaiyolkh@users.noreply.github.com>
2022-01-14 07:01:56 -0500

    Update whatsappautomator.py

--- whatsappautomator.py
+++ whatsappautomator.py
@@ -6,14 +6,16 @@ print("Whatsapp message automator made by Kyle Lam on January 13, 2022, Welcome!
 phonenum = str(input("Please enter the phone number of the contact you want to send the message to: "))
 msg = str(input("Please enter the message you want to send: "))
 hr = int(input("Please enter the hour you want this message to send (Use 24 hour format): "))
-min = int(input("Please enter the minute you want this message to send: "))
+minute = int(input("Please enter the minute you want this message to send: "))
 
-if 0 <= hr <= 9:
+# The if statements below basically just makes the output look better
+# For example if you want a message to be delivered at 12am, the output would show 00:00 instead of 0:0
+if 0 <= hr <= 9: 
     hr = "0"+str(hr)
-if 0 <= min <= 9:
-    min = "0"+str(min)
+if 0 <= minute <= 9:
+    minute = "0"+str(minute)
 
-print("\nThank you for using the Whatsapp message automator! The message will be delivered to " + phonenum + " at " + str(hr) + ":" + str(min))
+print("\nThank you for using the Whatsapp message automator! The message will be delivered to " + phonenum + " at " + str(hr) + ":" + str(minute))
 
-pywhatkit.sendwhatmsg(phonenum, msg, int(hr), int(min))
-# syntax: pywhatkit.sendwhatmsg("area code and phone number", "message", time in hours, time in minutes)
+pywhatkit.sendwhatmsg(phonenum, msg, int(hr), int(minute))
+# Syntax: pywhatkit.sendwhatmsg("area code and phone number", "message", time (hour), time (minute))
